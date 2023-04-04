3fc647834641e9678d7630f340e71f1b02b22730
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-04-03 23:03:56 -0400

    building the response data

--- src/WordApi.js
+++ src/WordApi.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./WordApi.css";
 
 export default function WordApi(props) {
-  const [description, setDescription] = useState();
+  let [description, setDescription] = useState();
   let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${props.word}`;
 
   function showDescription(response) {
     console.log(response);
+    setDescription({
+      word: props.word,
+      definition: response.data[0].meaning[0].definition[0].definition,
+    });
   }
   axios.get(apiUrl).then(showDescription);
-  return "";
+  description = "hi";
+  return (
+    <div className="WordApi">
+      <div className="Container">
+        <h2>{props.word}</h2>
+        <p>{description.definition}</p>
+      </div>
+    </div>
+  );
 }
