128b22cf08f3dce2518eb134234ee8da17630fc8
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-04-10 12:13:30 -0400

    words example updated

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -117,9 +117,9 @@ export default function Dictionary(props) {
           </div>
           <div className="Example">
             <label>
-              example: rain, book, color, moon, sky, lens, rice, pencil, dog,
-              grass, cloud, people, first, it, would, write, computer, coding,
-              game, could, way, car, object, animal, plant, sun, scorpion.
+              i.e: rain, book, color, moon, sky, lens, rice, pencil, dog, grass,
+              cloud, people, first, it, would, write, computer, coding, game,
+              could, way, car, object, animal, plant, sun, scorpion.
             </label>
           </div>
           <div className="WordsContainer">
